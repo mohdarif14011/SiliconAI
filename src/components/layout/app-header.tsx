294475f6df015c545afe1910cd1810a6f02885a5
Mohd Arif <mohdarif14011@gmail.com>
2025-10-16 05:36:17 +0000

    There is most frustating error happening, write the whole new corrected

--- src/components/layout/app-header.tsx
+++ src/components/layout/app-header.tsx
@@ -230,8 +230,8 @@ const ListItem = React.forwardRef<
     <li>
       <NavigationMenuLink asChild>
         <Link
-          ref={ref}
           href={href!}
+          ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-2 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground text-sm",
             className
@@ -248,3 +248,5 @@ const ListItem = React.forwardRef<
   );
 });
 ListItem.displayName = "ListItem";
+
+    
