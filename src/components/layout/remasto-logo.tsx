6f2f1795f3480407828306b5d7af248669920a17
Mohd Arif <mohdarif14011@gmail.com>
2025-10-16 04:52:23 +0000

    Change the logo and name from here also

--- src/components/layout/remasto-logo.tsx
+++ src/components/layout/remasto-logo.tsx
@@ -4,12 +4,11 @@ export function RemastoLogo() {
     <div className="flex items-center">
       <img 
         src="https://res.cloudinary.com/donszbe80/image/upload/v1760549926/Logo_si_ygvrru.png" 
-        width="50"
-        height="14"
-        alt="SiliconAI Logo"
+        width="32"
+        height="32"
+        alt="VLSI Interview Ace Logo"
         style={{
-          borderRadius: "50%",
-          marginLeft: "8px"
+          borderRadius: "8px"
         }}
       />
       <div
@@ -21,10 +20,10 @@ export function RemastoLogo() {
           fontWeight: "bold",
           fontSize: "1.25rem",
           letterSpacing: "-0.05em",
-          marginLeft: "4px"
+          marginLeft: "8px"
         }}
       >
-        SiliconAI
+        VLSI Interview Ace
       </div>
     </div>
   );
