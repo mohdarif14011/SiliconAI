b6873103e0cddc81370571865cf846a48a711cbc
Mohd Arif <mohdarif14011@gmail.com>
2025-10-16 08:55:04 +0000

    import { cn } from "@/lib/utils"; const si_logo = "../public/favicon.svg

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -2,22 +2,7 @@
 import { cn } from "@/lib/utils";
 
 export const Logo = ({ className }: { className?: string }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    className={cn("fill-current", className)}
-  >
-    <circle cx="12" cy="12" r="12" fill="black" />
-    <text
-      x="12"
-      y="16"
-      fontFamily="Arial, sans-serif"
-      fontSize="12"
-      fontWeight="bold"
-      fill="white"
-      textAnchor="middle"
-    >
-      Si
-    </text>
-  </svg>
+  <div className={cn(className)}>
+    <img width="45px" src="/favicon.svg" alt="SiliconAI Logo" />
+  </div>
 );
