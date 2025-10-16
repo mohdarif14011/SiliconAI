dd87404d3e45187b87a3220ac55f08ff0e23120b
Mohd Arif <mohdarif14011@gmail.com>
2025-10-16 13:15:53 +0000

    Remove the header

--- src/components/layout/logo.tsx
+++
@@ -1,8 +0,0 @@
-
-import { cn } from "@/lib/utils";
-
-export const Logo = ({ className }: { className?: string }) => (
-  <div className={cn(className)}>
-    <img width="45px" src="/favicon.svg" alt="SiliconAI Logo" />
-  </div>
-);
