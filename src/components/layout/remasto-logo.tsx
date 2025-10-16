22451e4eb3ae278bec73eee1c0676873cd95019e
Mohd Arif <mohdarif14011@gmail.com>
2025-10-16 04:44:17 +0000

    export function RemastoLogo() {   return (     <>       <img       src=

--- src/components/layout/remasto-logo.tsx
+++ src/components/layout/remasto-logo.tsx
@@ -1,43 +1,27 @@
+
 export function RemastoLogo() {
   return (
-    <svg
-      width="110"
-      height="32"
-      viewBox="0 0 110 32"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="text-primary"
-    >
-      <g clipPath="url(#clip0_103_2)">
-        <path
-          d="M0 8C0 3.58172 3.58172 0 8 0H24V32H8C3.58172 32 0 28.4183 0 24V8Z"
-          fill="currentColor"
-        />
-        <rect x="20" width="12" height="32" fill="currentColor" opacity="0.75" />
-        <rect x="28" width="12" height="32" fill="currentColor" opacity="0.5" />
-        <foreignObject x="38" y="0" width="72" height="32">
-          <div
-            xmlns="http://www.w3.org/1999/xhtml"
-            style={{
-              display: "flex",
-              alignItems: "center",
-              height: "100%",
-              color: "hsl(var(--foreground))",
-              fontWeight: "bold",
-              fontSize: "1.5rem",
-              letterSpacing: "-0.05em",
-              marginLeft: "4px"
-            }}
-          >
-            Remas
-          </div>
-        </foreignObject>
-      </g>
-      <defs>
-        <clipPath id="clip0_103_2">
-          <rect width="110" height="32" rx="8" fill="white" />
-        </clipPath>
-      </defs>
-    </svg>
+    <div className="flex items-center">
+      <img 
+        src="https://res.cloudinary.com/donszbe80/image/upload/v1760549926/Logo_si_ygvrru.png" 
+        width="110"
+        height="32"
+        alt="SiliconAI Logo"
+      />
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          height: "100%",
+          color: "hsl(var(--foreground))",
+          fontWeight: "bold",
+          fontSize: "1.5rem",
+          letterSpacing: "-0.05em",
+          marginLeft: "4px"
+        }}
+      >
+        SiliconAI
+      </div>
+    </div>
   );
 }
